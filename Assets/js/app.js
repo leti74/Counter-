d4444e51e44649d03f7939ac07acb0512247b6c7
leti74 <letiimar4@gmail.com>
2025-03-18 17:54:37 +0100

    delegazione dell'evento

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,27 +1,4 @@
 
-let calculation = 0;
-const counter = document.querySelector('.js-paragrafo')
-
-const incrementa = document.querySelector('.js-incrementa')
-incrementa.addEventListener('click', () => {
- calculation += 1;
- counter.innerHTML = calculation;
-})
-
-
-const decrementa = document.querySelector('.js-decrementa')
-decrementa.addEventListener('click', () => {
-  calculation -= 1
-  counter.innerHTML = calculation;
-})
-
-const resetta = document.querySelector('.js-resetta')
-resetta.addEventListener('click', () => {
-  calculation = 0;
-  counter.innerHTML = calculation;
-})
-
-
 const bottoneTema = document.querySelector('.js-cambio-tema')
  const body = document.body
   
@@ -29,7 +6,24 @@ const bottoneTema = document.querySelector('.js-cambio-tema')
   body.classList.toggle('tema-scuro')
  })
 
-
+ let calculation = 0;
+ const counter = document.querySelector('.js-paragrafo');
+ 
+ const buttonWrapper = document.querySelector('.sfondo');
+ buttonWrapper.addEventListener('click', (event) => {
+   const button = event.target;
+   
+   if (button.classList.contains('js-incrementa')) {
+     calculation += 1;
+   } else if (button.classList.contains('js-decrementa')) {
+     calculation -= 1;
+   } else if (button.classList.contains('js-resetta')) {
+     calculation = 0;
+   }
+   
+   counter.innerHTML = calculation;
+ });
+ 
 
 
 
